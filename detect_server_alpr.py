e14b3e5275d2fc72ca4d113bdbd9324d61b88b35
Yuting_Yen <jj861119@gmail.com>
2024-04-09 11:11:13 +0800

    Update detect_server_alpr.py
    1. fix path bug

--- detect_server_alpr.py
+++ detect_server_alpr.py
@@ -35,6 +35,12 @@ from pathlib import Path
 import json
 import torch
 
+import platform
+import pathlib
+plt = platform.system()
+if plt != 'Windows':
+  pathlib.WindowsPath = pathlib.PosixPath
+
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[0]  # YOLOv5 root directory
 if str(ROOT) not in sys.path:
